5e6a822a8c4f55d6434991fe7c7473a169483582
Nicolas Sassone <nico_distortion@hotmail.com>
2024-07-31 18:24:27 -0300

    cambios en el precio

--- src/components/ItemDetailContainer/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail.jsx
@@ -7,8 +7,11 @@ const ItemDetail = ( { producto } ) => {
     const productoCarrito = { ...producto, cantidad: contador }
 
     console.log(productoCarrito)
-  }
 
+  }
+  
+  
+  
   return (
     <div className="descripcion">
         <div className="descripcion2">
@@ -17,6 +20,7 @@ const ItemDetail = ( { producto } ) => {
         <div className="descripcion3">
             <h2>{producto.nombre}</h2>
             <p className="parrafo2">{producto.descripcion}</p>
+            <h4> $ {producto.precio}</h4> {/* Aquí mostramos el precio formateado */}
             <ItemCount stock={producto.stock} agregarAlCarrito={agregarAlCarrito} />
         </div>
     </div>
--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -2,10 +2,8 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Item = ({ producto }) => {
-  
     // Formatear el precio con puntos como separadores de miles
     const precioFormateado = producto.precio.toLocaleString();
-
   return (
     <div className=" card"  >
       <img width={200} src={producto.imagen} alt="" />
