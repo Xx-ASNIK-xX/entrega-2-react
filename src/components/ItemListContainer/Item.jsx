35ac4f0b71215869472797a359f0d601f77ea130
Nicolas Sassone <nico_distortion@hotmail.com>
2024-07-31 14:40:07 -0300

    se agrego puntos a los precios

--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 
 const Item = ({ producto }) => {
   
+    // Formatear el precio con puntos como separadores de miles
+    const precioFormateado = producto.precio.toLocaleString();
+
   return (
     <div className=" card"  >
       <img width={200} src={producto.imagen} alt="" />
@@ -11,6 +14,7 @@ const Item = ({ producto }) => {
       <br></br>
       <p>{producto.descripcion}</p>
       <br></br>
+      <h4>{precioFormateado}</h4> {/* Aquí mostramos el precio formateado */}
       <br></br>
       <Link to={"/detalle/" + producto.id}>
         Ver detalles
--- src/data/data.js
+++ src/data/data.js
@@ -37,9 +37,9 @@ const productos = [
     },
     {
         id: "Gilera2",
-        nombre: "Gilera Smash 110 Automatica",
-        descripcion: "",
-        precio: 500,
+        nombre: "Gilera Smash 110",
+        descripcion: "Cilindrada, 107 cc ; Freno a tambor. Transmisión, automatica. Capacidad del tanque de 4,1 L.",
+        precio: 1348000,
         categoria: "Scooter",
         stock: 10,
         imagen: "/img/moto4.jpg"
@@ -47,7 +47,8 @@ const productos = [
     {
         id: "Hero2",
         nombre: "Hero Dash 125 con Baul",
-        descripcion: "",
+        descripcion: "Cilindrada, 125 cc ; Freno a tambor. Transmisión, automatica. Capacidad del tanque de 2,8 L.",
+        
         precio: 1348000,
         categoria: "Scooter",
         stock: 5,
@@ -56,7 +57,7 @@ const productos = [
     {
         id: "Yamaha2",
         nombre: "Yamaha Fascino 125",
-        descripcion: "",
+        descripcion: "Cilindrada, 125 cc ; Freno a disco delantero. Transmisión, automatica. Capacidad del tanque de 5,2 L.",
         precio: 3098000,
         categoria: "Scooter",
         stock: 10,
@@ -65,7 +66,7 @@ const productos = [
     {
         id: "Honda2",
         nombre: "Honda PCX 160 2023",
-        descripcion: "",
+        descripcion: "Cilindrada, 156,93 cc ; Freno a disco delantero. Transmisión, automatica. Capacidad del tanque de 7,7 L.",
         precio: 7198000,
         categoria: "Scooter",
         stock: 7,
@@ -74,7 +75,7 @@ const productos = [
     {
         id: "Keller3",
         nombre: "Keller Miracle 150",
-        descripcion: "",
+        descripcion: "Cilindrada, 149 cc ; Freno a tambor. Transmisión manual. Capacidad del tanque de 11 L.",
         precio: 1898000,
         categoria: "Enduro",
         stock: 4,
@@ -83,7 +84,7 @@ const productos = [
     {
         id: "Zanella3",
         nombre: "Zanella ZT 150",
-        descripcion: "",
+        descripcion: "Cilindrada, 145 cc ; Freno a disco. Transmisión manual. Capacidad del tanque de 12 L.",
         precio: 2098000,
         categoria: "Enduro",
         stock: 5,
@@ -92,7 +93,7 @@ const productos = [
     {
         id: "Yamaha3",
         nombre: "Yamaha XTZ 125",
-        descripcion: "",
+        descripcion: "Cilindrada, 124 cc ; Freno a disco. Transmisión manual. Capacidad del tanque de 10,6 L.",
         precio: 4498000,
         categoria: "Enduro",
         stock: 9,
@@ -101,7 +102,7 @@ const productos = [
     {
         id: "Honda3",
         nombre: "Honda XR 250",
-        descripcion: "",
+        descripcion: "Cilindrada, 149 cc ; Freno a disco. Transmisión manual. Capacidad del tanque de 23 L.",
         precio: 6598000,
         categoria: "Enduro",
         stock: 6,
